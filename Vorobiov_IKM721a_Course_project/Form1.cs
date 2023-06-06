8b71f497076af45c7e4b9f8c1218d2e374ac2afa
Vorobiov Dmytro <dimson124@gmail.com>
2023-06-06 12:40:35 +0300

    Finish Task number 2

--- Vorobiov_IKM721a_Course_project/Form1.cs
+++ Vorobiov_IKM721a_Course_project/Form1.cs
@@ -13,6 +13,7 @@ namespace Vorobiov_IKM721a_Course_project
     public partial class Form1 : Form
     {
         private bool Mode; // Режим дозволу / заборони введення даних
+        private MajorWork MajorObject; // Створення об'єкта класу MajorWork
 
         public Form1()
         {
@@ -29,6 +30,10 @@ namespace Vorobiov_IKM721a_Course_project
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            About A = new About(); // створення форми About
+            A.tAbout.Start();
+            A.ShowDialog(); // відображення діалогового вікна About
+            MajorObject = new MajorWork();
             this.Mode = true;
         }
 
@@ -48,6 +53,9 @@ namespace Vorobiov_IKM721a_Course_project
                 tClock.Stop();
                 bStart.Text = "Пуск";// зміна тексту на кнопці на "Пуск"
                 this.Mode = true;
+                MajorObject.Write(tbInput.Text);// Запис даних у об'єкт
+                MajorObject.Task();// Обробка даних
+                label1.Text = MajorObject.Read();// Відображення результату
             }
         }
 
